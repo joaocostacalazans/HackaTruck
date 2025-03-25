21717fece7933b0ab04fb1d24b022f625dffd77e
João Costa Calazans <costacalazansjoao@gmail.com>
2025-03-25 16:53:51 -0300

    Add files via upload

---
+++ desafio08/desafio08/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  desafio08
+//
+//  Created by Turma01-10 on 25/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ desafio08/desafio08/desafio08App.swift
@@ -0,0 +1,17 @@
+//
+//  desafio08App.swift
+//  desafio08
+//
+//  Created by Turma01-10 on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct desafio08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
